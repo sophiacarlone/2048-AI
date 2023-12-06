1606b605895c586807d2002fea824f58c298aefa
iselda aiello <aielloise@gmail.com>
2023-12-06 15:23:36 -0500

    test more functions

--- test.cpp
+++ test.cpp
@@ -4,20 +4,31 @@ using namespace std;
 
 int main()
 {
+	ofstream test;
+	test.open( "test.txt" );
+
 	Grid g;
 	g.print();
-	g.moveLeft();
+
+	g = g.moveLeft();
 	cout << "moving left..." << endl;
 	g.print();
-	g.moveUp();
+	g.print_to_stream( test );
+
+	g = g.moveUp();
 	cout << "moving up..." << endl;
 	g.print();
-	g.moveRight();
+	g.print_to_stream( test );
+
+	g = g.moveRight();
 	cout << "moving right..." << endl;
 	g.print();
-	g.moveDown();
+	g.print_to_stream( test );
+
+	g = g.moveDown();
 	cout << "moving down..." << endl;
 	g.print();
+	g.print_to_stream( test );
 
 	return 0;
 }
