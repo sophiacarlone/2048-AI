f068070deab3aa58a16d023f01af7ee1fe91bd43
orourkrl <orourkrl@clarkson.edu>
2023-12-06 16:07:22 -0500

    More changes to ids

--- ids.cpp
+++ ids.cpp
@@ -6,33 +6,33 @@ using namespace std;
 
 // Built around max tree
 
-int ids(int maxDepth, Grid start, Grid goal){
+Grid ids(int maxDepth, Grid start, Grid goal){
     for (int depth = 0; depth <= maxDepth; depth++){
-        vector<Grid> refVector();
+        vector<Grid> possibleMoves();
         stack<Grid> dfs;
         dfs.push(start);
 
-        while(!dfs.empty()){
-            Grid top = dfs.top();
-            dfs.pop();
-            for (int i = 0; i < 16; i++){
-                if (top.blocks[i].num == 2048){ 
-                    cout << "2048 YIPEE";
-                    top.print();
-                    return 0;
-                }
+        
+        Grid top = dfs.top();
+        dfs.pop();
+        for (int i = 0; i < 16; i++){
+            if (top.blocks[i].num == 2048){ 
+                cout << "2048 YIPEE";
+                top.print();
+                return top;
             }
-            int highScore = top.score; //not real function yet, meant to get highest value block
-            for (int i = 0; i < refVector.size(); i++){ // Fix vector initialization
-                if (refVector[i].score > highScore){
-                    Grid temp = top;
-                    dfs.push(temp);
-                }
-            }
-            cout << "Removing: ";
-            top.print();
         }
+        int highVal = top.value(); //not real function yet, meant to get highest value block
+        for (int i = 0; i < 4; i++){ // Fix vector initialization
+            if (possibleMoves[i].value() > highVal){
+                Grid temp = top;
+                dfs.push(temp);
+            }
+        }
+        cout << "Removing: ";
+        top.print();
     }
     cout << "Failure, game impossible to win";
-    return 0;
+    return;
 }
+
